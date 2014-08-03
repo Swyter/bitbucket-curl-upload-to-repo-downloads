5f77705bda4fe7a9e85c95ce8e6a200bb50f6bf3
Swyter <690187+Swyter@users.noreply.github.com>
2014-08-03 20:56:38 +0200

    *tweaked, tested and now working on lin/unixes.

--- upload-to-bitbucket.sh
+++ upload-to-bitbucket.sh
@@ -17,29 +17,30 @@ fi
 	# GET initial csrf, dropped in the cookie, final 32 chars of the line containing that word
 	# [i] note: you can add the "-v" parameter to any cURL command to get a detailed/verbose output, useful to diagnose problems.
 	echo "getting initial csrf token from the sign-in page:"
-	curl -k -c cookies.txt --progress-bar -o nul https://bitbucket.org/account/signin/
+	curl -k -c cookies.txt --progress-bar -o /dev/null https://bitbucket.org/account/signin/
 	
-	csrf=$(grep csrf cookies.txt  | sed -r "s/.+\s([^\s]+)$/\1/")
+	csrf=$(grep csrf cookies.txt); set $csrf; csrf=$7;
 	
 	# and login using POST, to get the final session cookies, then redirect it to the right page
 	echo "signing in with the credentials provided:"
-	curl -k -c cookies.txt -b cookies.txt --progress-bar -o nul -d \"username\=$usr\&password\=$pwd\&submit\=\&next\=$pge\&csrfmiddlewaretoken\=$csrf\" --referer "https://bitbucket.org/account/signin/" -L https://bitbucket.org/account/signin/
+	curl -k -c cookies.txt -b cookies.txt --progress-bar -o /dev/null -d "username=$usr&password=$pwd&submit=&next=$pge&csrfmiddlewaretoken=$csrf" --referer "https://bitbucket.org/account/signin/" -L https://bitbucket.org/account/signin/
 	
-	csrf=$(grep csrf cookies.txt  | sed -r "s/.+\s([^\s]+)$/\1/")
+	csrf=$(grep csrf cookies.txt); set $csrf; csrf=$7;
 	
 	# check that we have the session cookie, if not, something bad happened, don't spend time uploading.
-if [ $(grep bb_session cookies.txt) -eq "" ]; then
+
+if [ -z "$(grep bb_session cookies.txt)" ]; then
 	cat <<-EOF
 	
 	[!] error: didn't get the session cookie, probably bad credentials or they changed stuff... upload canceled!
 	EOF
-	
+
 	exit
 fi
-	
+
 	# now that we're logged-in and at the right page, upload whatever you want to your repository...
 	echo "actual upload progress should appear right now as a progress bar, be patient:"
-	curl -k -c cookies.txt -b cookies.txt --progress-bar -o nul --referer \"https://bitbucket.org/$pge\" -L --form csrfmiddlewaretoken=$csrf --form token= --form file=@\"$fil\" https://bitbucket.org/$pge
-	
+	curl -k -c cookies.txt -b cookies.txt --progress-bar -o /dev/null --referer "https://bitbucket.org/$pge" -L --form csrfmiddlewaretoken=$csrf --form token= --form file=@"$fil" https://bitbucket.org/$pge
+
 	echo "done? maybe. *crosses fingers* signing out, closing session!"
-	curl -k -c cookies.txt -b cookies.txt --progress-bar -o nul -L https://bitbucket.org/account/signout/
+	curl -k -c cookies.txt -b cookies.txt --progress-bar -o /dev/null -L https://bitbucket.org/account/signout/
